50e9c61357ea353b246b06b0e9db2572cd58483b
titesen <zgf.nicolas@gmail.com>
2026-03-19 21:15:02 -0300

    Refactor code structure for improved readability and maintainability

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -5,7 +5,26 @@ const config: Config = {
   rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.(t|j)s$': [
+      '@swc/jest',
+      {
+        sourceMaps: 'inline',
+        module: {
+          type: 'commonjs',
+        },
+        jsc: {
+          target: 'es2022',
+          parser: {
+            syntax: 'typescript',
+            decorators: true,
+          },
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+        },
+      },
+    ],
   },
   collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
